a46e4ac3fdfdef1c76d11074415166f68c1006c0
Tsikhan Navitski <tikhon.trans@gmail.com>
2023-04-09 15:12:34 +0300

    Initial commit

---
+++ src/main/java/by/tnavitski/testbeatdev/TestBeatdevApplication.java
@@ -0,0 +1,13 @@
+package by.tnavitski.testbeatdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBeatdevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBeatdevApplication.class, args);
+    }
+
+}
